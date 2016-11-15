ab90d3f3ca6e4c312a5947bab511f9fe2c5aca76
BadMonkey <328620988@qq.com>
2016-11-15 12:37:41 +0800

    showdialog submit

--- app/src/main/java/com/mk/workdemo/MainActivity.java
+++ app/src/main/java/com/mk/workdemo/MainActivity.java
@@ -3,12 +3,15 @@ package com.mk.workdemo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.mk.workdemo.Activities.SecondAcitivity;
 import com.mk.workdemo.Activities.TitleActivity;
 import com.mk.workdemo.Utils.LogUtils;
+import com.mk.workdemo.Utils.ShowDialog;
 
-public class MainActivity extends TitleActivity {
+public class MainActivity extends TitleActivity implements View.OnClickListener {
+    private String[] dia = new String[]{"wo",".","数量"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +21,63 @@ public class MainActivity extends TitleActivity {
 //        setLeftAndIcon("后退", R.drawable.ic_action_add, false);
         setRightText("123");
         setRightFirstDrawable(R.drawable.ic_action_exit);
-        findViewById(R.id.tv_jump).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SecondAcitivity.class));
-            }
-        });
         LogUtils.d("我在MainActivity中");
+        findViewById(R.id.tv_dialog_confirm).setOnClickListener(this);
+        findViewById(R.id.tv_dialog_alert_one).setOnClickListener(this);
+        findViewById(R.id.tv_dialog_alert_two).setOnClickListener(this);
+        findViewById(R.id.tv_dialog_edit).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_jump:
+                startActivity(new Intent(MainActivity.this, SecondAcitivity.class));
+                break;
+            case R.id.tv_dialog_confirm:
+                ShowDialog.showConfiromDialog(this, "标题", "内容", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,"点击了确定",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                break;
+            case R.id.tv_dialog_alert_one:
+                ShowDialog.showSelectDialog(this, "选择1", "选择内容1", "哈哈哈", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,"选择1",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                break;
+            case R.id.tv_dialog_alert_two:
+                ShowDialog.showSelectDialog(this, "选择2", "选择内容2", "", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,"点击了确定",Toast.LENGTH_SHORT).show();
+                    }
+                }, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,"点击了取消",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                break;
+            case R.id.tv_dialog_edit:
+                ShowDialog.showEditDialog(this, "我是标题", "我是内容", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,""+view.getTag(),Toast.LENGTH_SHORT).show();
+//                        LogUtils.d(view.getTag());
+                    }
+                },dia);
+                break;
+        }
 
     }
 }
--- app/src/main/java/com/mk/workdemo/Utils/ShowDialog.java
+++ app/src/main/java/com/mk/workdemo/Utils/ShowDialog.java
@@ -1,8 +1,197 @@
 package com.mk.workdemo.Utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.mk.workdemo.R;
+
 /**
  * MK on 2016/11/11.
  */
 
 public class ShowDialog {
+    public static void showConfiromDialog(Context context, String title, String message,
+                                          final View.OnClickListener listener){
+        final AlertDialog dialog = new AlertDialog.Builder(context, R.style.dialogStyle)
+                .setCancelable(false).create();
+        if(((Activity)context).isFinishing()){
+            return;
+        }
+        dialog.show();
+        dialog.setContentView(R.layout.dialog_confirom);
+        TextView mTitle = (TextView) dialog.getWindow().findViewById(R.id.tv_dialog_confirom_title);
+        TextView mContent = (TextView) dialog.getWindow().findViewById(R.id.tv_dialog_confirom_content);
+        TextView mOk = (TextView) dialog.getWindow().findViewById(R.id.tv_dialog_confirom_ok);
+        mTitle.setText(title);
+        mContent.setText(message);
+        dialog.getWindow().findViewById(R.id.tv_dialog_confirom_ok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null){
+                    listener.onClick(view);
+                }
+                dialog.dismiss();
+            }
+        });
+        dialog.getWindow().findViewById(R.id.tv_dialog_confirom_cancle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+    public static void showSelectDialog(Context context, String title, String message,
+                                        String msgSub, final View.OnClickListener listener) {
+        final AlertDialog alertDialog = new AlertDialog.Builder(context, R.style.dialogStyle)
+                .setCancelable(false).create();
+        if (((Activity) context).isFinishing()) {
+            return;
+        }
+        alertDialog.show();
+        alertDialog.setContentView(R.layout.dialog_alert);
+        TextView tvTitle = (TextView) alertDialog.getWindow().findViewById(
+                R.id.tv_edit_dialog_title);
+        TextView tvMsg = (TextView) alertDialog.getWindow().findViewById(R.id.tv_select_dialog_msg);
+        TextView tvSubMsg = (TextView) alertDialog.getWindow().findViewById(
+                R.id.tv_select_dialog_submsg);
+        tvTitle.setText(title);
+        tvMsg.setText(message);
+        tvSubMsg.setText(msgSub);
+        if (TextUtils.isEmpty(message)) {
+            tvMsg.setVisibility(View.GONE);
+        }
+        if (TextUtils.isEmpty(msgSub)) {
+            tvSubMsg.setVisibility(View.GONE);
+        }
+        alertDialog.getWindow().findViewById(R.id.tv_select_dialog_cancel).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+        alertDialog.getWindow().findViewById(R.id.tv_select_dialog_ok).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        listener.onClick(view);
+                        alertDialog.dismiss();
+                    }
+                });
+    }
+
+    public static void showSelectDialog(Context context, String title, String message,
+                                        String msgSub, final View.OnClickListener listener,
+                                        final View.OnClickListener dealCancel) {
+        final AlertDialog alertDialog = new AlertDialog.Builder(context, R.style.dialogStyle)
+                .setCancelable(false).create();
+        if (((Activity) context).isFinishing()) {
+            return;
+        }
+        alertDialog.show();
+        alertDialog.setContentView(R.layout.dialog_alert);
+        TextView tvTitle = (TextView) alertDialog.getWindow().findViewById(
+                R.id.tv_edit_dialog_title);
+        TextView tvMsg = (TextView) alertDialog.getWindow().findViewById(R.id.tv_select_dialog_msg);
+        TextView tvSubMsg = (TextView) alertDialog.getWindow().findViewById(
+                R.id.tv_select_dialog_submsg);
+        tvTitle.setText(title);
+        tvMsg.setText(message);
+        tvSubMsg.setText(msgSub);
+        if (TextUtils.isEmpty(message)) {
+            tvMsg.setVisibility(View.GONE);
+        }
+        if (TextUtils.isEmpty(msgSub)) {
+            tvSubMsg.setVisibility(View.GONE);
+        }
+        alertDialog.getWindow().findViewById(R.id.tv_select_dialog_cancel).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                view.setTag(alertDialog);
+                dealCancel.onClick(view);
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.getWindow().findViewById(R.id.tv_select_dialog_ok).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                view.setTag(alertDialog);
+                listener.onClick(view);
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+
+    public static void showEditDialog(Context context, String title, String message,
+                                      View.OnClickListener listener) {
+        showEditDialog(context, title, message, listener, new String[]{"请输入原因"});
+    }
+
+    public static void showEditDialog(Context context, String title, String message,
+                                      final View.OnClickListener listener, final String[] hint) {
+        final AlertDialog alertDialog = new AlertDialog.Builder(context, R.style.dialogStyle)
+                .setCancelable(false).create();
+        if (((Activity) context).isFinishing()) {
+            return;
+        }
+        alertDialog.show();
+        alertDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        alertDialog.setContentView(R.layout.dialog_edit);
+        TextView tvTitle = (TextView) alertDialog.getWindow().findViewById(
+                R.id.tv_edit_dialog_title);
+        TextView tvMsg = (TextView) alertDialog.getWindow().findViewById(R.id.tv_edit_dialog_msg);
+        final LinearLayout llContainer = (LinearLayout) alertDialog.getWindow().findViewById(
+                R.id.ll_edit_dialog_container);
+
+        for (int i = 0; i < hint.length; i++) {
+            llContainer.getChildAt(i).setVisibility(View.VISIBLE);
+            ((EditText) llContainer.getChildAt(i)).setHint(hint[i]);
+            //输入数字
+            if (hint[i].contains(".")) {
+                ((EditText) llContainer.getChildAt(i)).setInputType(
+                        InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            }
+            if (hint[i].contains("数量")) {
+                ((EditText) llContainer.getChildAt(i)).setInputType(InputType.TYPE_CLASS_NUMBER);
+            }
+        }
+        tvTitle.setText(title);
+        tvMsg.setText(message);
+        if (TextUtils.isEmpty(message)) {
+            tvMsg.setVisibility(View.GONE);
+        }
+        alertDialog.getWindow().findViewById(R.id.tv_edit_dialog_cancel).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.getWindow().findViewById(R.id.tv_edit_dialog_ok).setOnClickListener(new View
+                .OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hint.length; i++) {
+                    sb.append(((EditText) llContainer.getChildAt(i)).getText().toString()).append("!@#");
+                }
+                view.setTag(sb.toString());
+                listener.onClick(view);
+                alertDialog.dismiss();
+            }
+        });
+    }
 }
